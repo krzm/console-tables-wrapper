600b6099d966fc9d7fe65e52f767615b94daf5fc
krzm <kmazanek@gmail.com>
2022-07-16 17:46:58 +0200

    setup console-tables project

---
+++ Console.Tables.Wrapper.TestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Console.Tables.Wrapper;
+using ConsoleTables;
+using Output = System.Console;
+
+var table = new ConsoleTable("one", "two", "three");
+table.AddRow(1, 2, 3)
+     .AddRow("this line should be longer", "yes it is", "oh");
+table.Write();
+Output.WriteLine();
+
+var rows = Enumerable.Repeat(new Sample(), 10);
+
+ConsoleTable
+    .From<Sample>(rows)
+    .Configure(o => o.NumberAlignment = Alignment.Right)
+    .Write(Format.Alternative);
+
+Output.ReadKey();
---
+++ Console.Tables.Wrapper/Sample.cs
@@ -0,0 +1,16 @@
+﻿namespace Console.Tables.Wrapper;
+
+public class Sample
+{
+    public Sample()
+    {
+        Id = Guid.NewGuid().ToString("N");
+        Name = "Planet earth";
+        Date = DateTime.Now;
+    }
+
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public int PopulationNumber { get; set; }
+}
